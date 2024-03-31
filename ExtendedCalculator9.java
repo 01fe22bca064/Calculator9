96d4f951fa80a81c4a8e648f0591048601898917
01fe22bca064 <01fe22bca064@kletech.ac.in>
2024-03-31 07:16:16 -0700

    code for square and cube functionality

---
+++ ExtendedCalculator9.java
@@ -0,0 +1,11 @@
+public class ExtendedCalculator9 extends Calculator9 {
+    public float Square(float num)
+    {
+        return(num * num); 
+    }
+    public float Cube(float num)
+    {
+        return(num * num * num); 
+    }
+    
+}
